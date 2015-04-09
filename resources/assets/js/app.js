987c3344919fc6095271d99edc8eb15a007743d4
Daniel H <frontend@reddesignsystems.com>
2015-04-09 09:42:54 -0500

    Config sweetalert2

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,10 @@
-$('li.warning.confirm button').on('click', function() {
+$('.btn-enviar').on('click', function() {
   swal({
-    title: 'Are you sure?',
-    text: 'You will not be able to recover this imaginary file!',
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
+    title: 'GRACIAS',
+    text: 'su mensaje ha sido enviado, pronto estaremos en contacto con usted.',
+    type: 'success',
     cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!',
-    closeOnConfirm: false
-  },
-  function() {
-    swal('Deleted!', 'Your file has been deleted!', 'success');
+    showConfirmButton: 'false',
+    width: '310',
   });
 });
