a754f1f03b5fb4cb62f78f018ad7ccc58deaac48
Daniel H <frontend@reddesignsystems.com>
2015-04-09 12:45:47 -0500

    Efect softener for btn-seeplans

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,24 @@
-$('.btn-enviar').on('click', function() {
-  swal({
-    title: 'GRACIAS',
-    text: 'su mensaje ha sido enviado, pronto estaremos en contacto con usted.',
-    type: 'success',
-    cancelButtonColor: '#d33',
-    showConfirmButton: 'false',
-    width: '310',
-  });
+$(function(){
+    $('#btnsee').click(function() {
+
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')&& location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+    $('.btn-enviar').on('click', function() {
+        swal({
+        	title: 'GRACIAS',
+        	text: 'su mensaje ha sido enviado, pronto estaremos en contacto con usted.',
+        	type: 'success',
+        	cancelButtonColor: '#d33',
+        	showConfirmButton: 'false',
+        	width: '310',
+        });
+    });
 });
